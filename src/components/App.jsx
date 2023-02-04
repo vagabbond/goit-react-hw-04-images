a4e14ad850db26619ac943a8e7113ddf5da116ce
Maks Thimbarov <imaksim2@icloud.com>
2023-02-04 19:43:51 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,6 @@ export const App = () => {
   const [filter, setFilter] = useState('');
   const [page, setPage] = useState(1);
   const [largeImg, setLargeImg] = useState('');
-
   const [lastFetchedFilter, setLastFetchedFilter] = useState(null);
   const [lastFetchedPage, setLastFetchedPage] = useState(null);
 
@@ -85,84 +84,3 @@ export const App = () => {
     </>
   );
 };
-// export class App extends Component {
-//   state = {
-//     images: [],
-//     status: 'idle',
-//     filter: '',
-//     page: 1,
-//     largeImg: '',
-//   };
-//   componentDidUpdate(prevProps, prevState) {
-//     if (
-//       prevState.filter !== this.state.filter ||
-//       prevState.page !== this.state.page
-//     ) {
-//       this.setState({ status: 'pending' });
-//       this.fetchImg(this.state.filter, this.state.page);
-//     }
-//     if (prevState.filter !== this.state.filter) {
-//       this.setState({ images: [] });
-//     }
-//   }
-
-// fetchImg = async (filter, page) => {
-//   try {
-//     const hits = await fetchImages(filter, page);
-//     hits.length === 0
-//       ? this.setState({ status: 'rejected' })
-//       : this.setState(prevState => ({
-//           images: [...prevState.images, ...hits],
-//           status: 'resolved',
-//         }));
-//   } catch (error) {
-//     console.log(error);
-//     this.setState({ status: 'rejected' });
-//   }
-//   };
-//   onSubmit = filter => {
-//     this.setState({ filter });
-//   };
-//   onClick = e => {
-//     e.preventDefault();
-//     this.setState(prevState => ({ page: prevState.page + 1 }));
-//   };
-//   openModal = img => {
-//     this.setState({ largeImg: img });
-//   };
-//   toggleModal = () => {
-//     this.setState({ largeImg: '' });
-//   };
-//   render() {
-//     const { status } = this.state;
-//     return (
-//       <>
-//         <SearchBar onSubmit={this.onSubmit}></SearchBar>
-//         {status === 'rejected' && <h1>Something went wrong</h1>}
-//         {this.state.images.length > 0 && (
-//           <Gallery images={this.state.images} onClick={this.openModal} />
-//         )}
-//         {status === 'pending' && (
-//           <ColorRing
-//             visible={true}
-//             height="80"
-//             width="80"
-//             ariaLabel="blocks-loading"
-//             wrapperStyle={{}}
-//             wrapperClass="blocks-wrapper"
-//             colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-//           />
-//         )}
-//         {status === 'resolved' && this.state.images.length > 0 && (
-//           <Button onClick={this.onClick} />
-//         )}
-//         {this.state.largeImg && (
-//           <Modal
-//             toggleModal={this.toggleModal}
-//             largeImg={this.state.largeImg}
-//           />
-//         )}
-//       </>
-//     );
-//   }
-// }
